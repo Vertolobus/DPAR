6fc630dbf6e962af815029c932e36fb4fe36f5b8
Vertolobus <samat.gaynullin@yandex.ru>
2025-04-19 01:10:09 +0300

    Доработка

--- Исключено/DPAR_log/log.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-// DPAR::log LOG FORMAT: (class-modifier [optional])[full_funcname] title : value[optional] << std::endl
-//EXAMPLE - "[DPAR::read] FILE-EXISTS : \"" << path << "\""
-
-void DPAR_log(std::string title = "", std::string full_funcname = "", std::string value = "", std::string class_modifier = "");
